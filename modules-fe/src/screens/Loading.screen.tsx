274d1d56b160c263101c8032e5a32d7901660b32
Rik Scheffer <r.scheffer@amsterdam.nl>
2024-02-28 17:04:08 +0100

    Fix duplicate header while loading

--- modules-fe/src/screens/Loading.screen.tsx
+++ modules-fe/src/screens/Loading.screen.tsx
@@ -1,14 +1,9 @@
-import Row from 'components/ui/layout/Row'
+import Loading from 'components/features/Loading'
 import Screen from 'components/ui/layout/Screen'
-import Icon from 'components/ui/media/Icon'
-import Phrase from 'components/ui/text/Phrase'
 
 const LoadingScreen = () => (
   <Screen>
-    <Row gutter="sm" valign="center">
-      <Icon name="spinner" />
-      <Phrase>Laden...</Phrase>
-    </Row>
+    <Loading />
   </Screen>
 )
 
