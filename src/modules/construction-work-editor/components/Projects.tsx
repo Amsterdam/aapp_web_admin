d546cd55ff664cff9b0bfced7c87f9d846edfd58
rogier83 <rogier@oberon.nl>
2024-04-18 18:10:49 +0200

    Create projects table and example

--- src/modules/construction-work-editor/components/Projects.tsx
+++ src/modules/construction-work-editor/components/Projects.tsx
@@ -1,62 +1,18 @@
-import {useCallback, useMemo, useState} from 'react'
+import {useCallback} from 'react'
 import {useNavigate} from 'react-router-dom'
 import ErrorComponent from 'components/ui/Error'
 import Loading from 'components/ui/Loading'
-import Image from 'components/ui/media/Image'
-import {Table} from 'components/ui/table/Table'
-import Phrase from 'components/ui/text/Phrase'
-import Title from 'components/ui/text/Title'
 import {ConstructionWorkEditorRoute} from 'modules/construction-work-editor/routes'
 import {useGetProjectsQuery} from 'modules/construction-work-editor/services'
-import {addSearchString, filterBySearchStringMatch} from 'utils/searchString'
-import type {ColumnConfig} from 'components/ui/table/types'
-import type {ProjectBase} from 'modules/construction-work-editor/types/project'
-
-const columns: ColumnConfig<ProjectBase>[] = [
-  {
-    key: 'title',
-    id: 'title',
-    renderer: ({title, subtitle}) => (
-      <>
-        <Title level={3}>{title}</Title>
-        <Phrase>{subtitle}</Phrase>
-      </>
-    ),
-    title: 'Titel',
-  },
-  {
-    key: 'image',
-    id: 'image',
-    renderer: ({image}) => image && <Image image={image} />,
-    title: '',
-  },
-]
+import ProjectsTable from './ProjectsTable'
+import type {ProjectsItem} from 'modules/construction-work-editor/types/project'
 
 const Projects = () => {
-  const [query, setQuery] = useState<string>()
   const navigate = useNavigate()
   const {data, isError, isLoading} = useGetProjectsQuery()
-  const projects = useMemo(
-    () =>
-      addSearchString(
-        data?.result?.map(project => ({
-          id: project.id,
-          title: project.title,
-          subtitle: project.subtitle,
-          image: project.image,
-        })),
-        ['title', 'subtitle'],
-      ),
-    [data?.result],
-  )
-
-  const filteredProjects = useMemo(
-    () => filterBySearchStringMatch<ProjectBase>(projects, query),
-    [projects, query],
-  )
 
   const handleRowClick = useCallback(
-    (project: ProjectBase) => {
+    (project: ProjectsItem) => {
       if (!project.id) return
       navigate(`${ConstructionWorkEditorRoute.project}/${project.id}`)
     },
@@ -67,19 +23,11 @@ const Projects = () => {
     return <Loading />
   }
 
-  if (isError || !projects?.length) {
+  if (isError || !data?.result?.length) {
     return <ErrorComponent message="Projecten kunnen niet worden getoond" />
   }
 
-  return (
-    <Table
-      config={columns}
-      data={filteredProjects}
-      filterCallback={setQuery}
-      keyGetter={({id}, affix = '') => `${affix}${id.toString()}`}
-      onRowClick={handleRowClick}
-    />
-  )
+  return <ProjectsTable projects={data.result} onRowClick={handleRowClick} />
 }
 
 export default Projects
