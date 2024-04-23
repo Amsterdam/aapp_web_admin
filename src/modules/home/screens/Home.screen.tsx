5cc5e9459046bd89cc7d4f4c563713fbceee1147
Frank Filius <f.filius@amsterdam.nl>
2024-04-23 13:04:44 +0200

    Move app login back into home screen

--- src/modules/home/screens/Home.screen.tsx
+++ src/modules/home/screens/Home.screen.tsx
@@ -1,19 +1,24 @@
-import NativeAppLogin from 'authentication/components/NativeAppLogin'
+import {LoginBoundary} from 'authentication/components/LoginBoundary'
+import {usePassTokenToNativeApp} from 'authentication/hooks/usePassTokenToNativeApp'
 import Column from 'components/ui/layout/Column'
 import Screen from 'components/ui/layout/Screen'
 import ScreenTitle from 'components/ui/text/ScreenTitle'
 import PersonalizedHome from 'modules/home/components/PersonalizedHome'
 
-const HomeScreen = () => (
-  <>
-    <NativeAppLogin />
+const HomeScreen = () => {
+  const isLoginApp = usePassTokenToNativeApp()
+  if (isLoginApp) {
+    return <LoginBoundary>Aan het inloggen...</LoginBoundary>
+  }
+
+  return (
     <Screen>
       <Column gutter="lg">
         <ScreenTitle title="Amsterdam App" />
         <PersonalizedHome />
       </Column>
     </Screen>
-  </>
-)
+  )
+}
 
 export default HomeScreen
