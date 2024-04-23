b4750b1169671334a81324d6232508d874dd06f6
Frank Filius <f.filius@amsterdam.nl>
2024-04-23 11:53:23 +0200

    Rename screen

---
+++ src/modules/construction-work-editor/screens/Publishers.screen.tsx
@@ -0,0 +1,12 @@
+import Screen from 'components/ui/layout/Screen'
+
+const PublishersScreen = () => (
+  <Screen>
+    <h1>Editors</h1>
+    <div>
+      <h2>Construction Work Editor</h2>
+    </div>
+  </Screen>
+)
+
+export default PublishersScreen
