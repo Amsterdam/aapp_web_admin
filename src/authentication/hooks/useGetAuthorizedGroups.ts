d59069ceb40e574871dacd785b9b35d73568211c
Frank Filius <f.filius@amsterdam.nl>
2024-04-25 09:54:10 +0200

    Loop over groups for authorization

--- src/authentication/hooks/useGetAuthorizedGroups.ts
+++ src/authentication/hooks/useGetAuthorizedGroups.ts
@@ -1,4 +1,4 @@
-import {useCallback} from 'react'
+import {useMemo} from 'react'
 import {azureGroups} from 'authentication/constants'
 import useGetDecodedAccessToken from 'authentication/hooks/useGetDecodedAccessToken'
 import {AzureGroup} from 'authentication/types'
@@ -7,26 +7,15 @@ import {environment} from 'utils/environment'
 const useGetAuthorizedGroups = () => {
   const decodedToken = useGetDecodedAccessToken()
   const {groups = []} = decodedToken ?? {groups: []}
+  const azureGroupNames = Object.keys(azureGroups) as AzureGroup[]
 
-  const getAuthorizedGroups = useCallback(
+  return useMemo(
     () =>
-      groups.reduce((acc, group) => {
-        if (group === azureGroups[AzureGroup.admin][environment]) {
-          return [...acc, AzureGroup.admin]
-        }
-        if (group === azureGroups[AzureGroup.editor][environment]) {
-          return [...acc, AzureGroup.editor]
-        }
-        if (group === azureGroups[AzureGroup.publisher][environment]) {
-          return [...acc, AzureGroup.publisher]
-        }
-
-        return acc
-      }, [] as AzureGroup[]),
-    [groups],
+      azureGroupNames.filter(groupName =>
+        groups.includes(azureGroups[groupName][environment]),
+      ),
+    [azureGroupNames, groups],
   )
-
-  return getAuthorizedGroups()
 }
 
 export default useGetAuthorizedGroups
