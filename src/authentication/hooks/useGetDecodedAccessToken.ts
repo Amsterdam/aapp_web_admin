e5cc719bd6bba32dc75e04d47cd2c1eeed1c50b6
Frank Filius <f.filius@amsterdam.nl>
2024-04-25 12:38:21 +0200

    Simplify hook

--- src/authentication/hooks/useGetDecodedAccessToken.ts
+++ src/authentication/hooks/useGetDecodedAccessToken.ts
@@ -1,34 +1,25 @@
 import {useMsal} from '@azure/msal-react'
 import {jwtDecode} from 'jwt-decode'
-import {useState, useEffect} from 'react'
+import {useEffect, useState} from 'react'
 import {DecodedJWTToken} from 'authentication/types'
 import {currentClientId} from 'utils/environment'
 
 const useGetDecodedAccessToken = () => {
   const {accounts, instance} = useMsal()
-  const [accessToken, setAccessToken] = useState('')
-  const [decodedToken, setDecodedToken] = useState<DecodedJWTToken>()
+  const [decodedAccessToken, setDecodedAccessToken] =
+    useState<DecodedJWTToken>()
 
   useEffect(() => {
-    const fetchData = async () => {
-      // Assuming you have a token in the cache
-      const response = await instance.acquireTokenSilent({
+    instance
+      .acquireTokenSilent({
         scopes: [`api://${currentClientId}/Modules.Edit`],
         account: accounts[0],
       })
-      setAccessToken(response.accessToken)
-    }
-
-    fetchData()
+      .then(({accessToken}) => {
+        setDecodedAccessToken(jwtDecode(accessToken))
+      })
   }, [accounts, instance])
 
-  useEffect(() => {
-    if (accessToken) {
-      const decoded = jwtDecode(accessToken)
-      setDecodedToken(decoded)
-    }
-  }, [accessToken])
-
-  return decodedToken
+  return decodedAccessToken
 }
 export default useGetDecodedAccessToken
