e96758e3e9190b94916450a914de979935bda465
Frank Filius <f.filius@amsterdam.nl>
2024-05-01 09:47:29 +0200

    Fix get url function and tests

--- src/utils/getUrl.ts
+++ src/utils/getUrl.ts
@@ -1,25 +1,25 @@
-/**
- * GetTypeOfParam<':id'> = {id: string}
- */
 type GetTypeOfParam<Param extends string> = Param extends `${infer Name}?`
   ? Partial<Record<Name, string>>
   : Record<Param, string>
 
-/**
- * ExtractParam<':id', '/articles/:articleId?'> = {id: string} & '/articles/:articleId?'
- */
 type ExtractParam<Path, NextPart> = Path extends `:${infer Param}`
   ? GetTypeOfParam<Param> & NextPart
   : NextPart
 
-/**
- * ExtractParams<'/:id/articles/:articleId?'> = {id: string, articleId?: string}
- */
 type ExtractParams<Path> = Path extends `${infer Segment}/${infer Rest}`
   ? ExtractParam<Segment, ExtractParams<Rest>>
   : ExtractParam<Path, Record<string, string>>
 
 const getUrl = <T extends string>(url: T, params: ExtractParams<T>) =>
-  url.replace(/:\w+/g, match => params[match.slice(1)])
+  url.replace(/:([^/?]+)\??/g, match => {
+    const paramName = match.replace(/[:?]/g, '')
+
+    return params[paramName] ?? ''
+  })
+
+const url = '/users/:userId/profile/:section?'
+const params = {userId: '123', section: 'settings'}
+
+getUrl(url, params)
 
 export default getUrl
