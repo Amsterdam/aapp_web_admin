981e262672dee682b08c4f818636ec73242062a4
Frank Filius <f.filius@amsterdam.nl>
2024-05-23 14:15:15 +0200

    Show publishers button only for editors

--- src/modules/construction-work-editor/components/ConstructionWorkEditorHome.tsx
+++ src/modules/construction-work-editor/components/ConstructionWorkEditorHome.tsx
@@ -1,20 +1,28 @@
+import useGetAuthorizedGroups from 'authentication/hooks/useGetAuthorizedGroups'
+import {AzureGroup} from 'authentication/types'
 import NavigationButton from 'components/ui/button/NavigationButton'
 import Row from 'components/ui/layout/Row'
 import {ConstructionWorkEditorRoute} from 'modules/construction-work-editor/types/routes'
 
-const ConstructionWorkEditorHome = () => (
-  <Row gutter="md">
-    <NavigationButton
-      flex
-      label="Werkzaamheden"
-      url={ConstructionWorkEditorRoute.projects}
-    />
-    <NavigationButton
-      flex
-      label="Publishers"
-      url={ConstructionWorkEditorRoute.publishers}
-    />
-  </Row>
-)
+const ConstructionWorkEditorHome = () => {
+  const authorizedGroups = useGetAuthorizedGroups()
+
+  return (
+    <Row gutter="md">
+      <NavigationButton
+        flex
+        label="Werkzaamheden"
+        url={ConstructionWorkEditorRoute.projects}
+      />
+      {authorizedGroups.includes(AzureGroup.editor) && (
+        <NavigationButton
+          flex
+          label="Publishers"
+          url={ConstructionWorkEditorRoute.publishers}
+        />
+      )}
+    </Row>
+  )
+}
 
 export default ConstructionWorkEditorHome
