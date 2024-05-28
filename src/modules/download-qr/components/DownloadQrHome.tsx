b130a417bc6dfcab2ee77c769fe0a6d9b9fffa15
Frank Filius <f.filius@amsterdam.nl>
2024-05-28 11:00:55 +0200

    Update download button

--- src/modules/download-qr/components/DownloadQrHome.tsx
+++ src/modules/download-qr/components/DownloadQrHome.tsx
@@ -1,15 +1,11 @@
 import NavigationButton from 'components/ui/button/NavigationButton'
-import Row from 'components/ui/layout/Row'
 import {DownloadQRCodeRoute} from 'modules/download-qr/types'
 
 const DownloadQrHome = () => (
-  <Row gutter="md">
-    <NavigationButton
-      flex
-      label="Download link QR code maker"
-      url={DownloadQRCodeRoute.downloadQRCode}
-    />
-  </Row>
+  <NavigationButton
+    label="Download link QR code maker"
+    url={DownloadQRCodeRoute.downloadQRCode}
+  />
 )
 
 export default DownloadQrHome
