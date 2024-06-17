d62ec0e49ca68a7d451237a0a01451ab42f9f41a
Frank Filius <f.filius@amsterdam.nl>
2024-06-17 13:22:14 +0200

    Improve edit and new publisher logic

--- src/modules/construction-work-editor/components/Publisher/CreatePublisher.tsx
+++ src/modules/construction-work-editor/components/Publisher/CreatePublisher.tsx
@@ -1,7 +1,9 @@
 import {Paragraph} from '@amsterdam/design-system-react'
+import NavigationButton from 'components/ui/button/NavigationButton'
 import Column from 'components/ui/layout/Column'
 import ScreenTitle from 'components/ui/text/ScreenTitle'
 import PublisherForm from 'modules/construction-work-editor/components/Publisher/PublisherForm'
+import {ConstructionWorkEditorRoute} from 'modules/construction-work-editor/types/routes'
 
 const CreatePublisher = () => (
   <Column gutter="lg">
@@ -11,6 +13,11 @@ const CreatePublisher = () => (
       volgende stap kun je projecten kiezen.
     </Paragraph>
     <PublisherForm />
+    <NavigationButton
+      label="Annuleren"
+      url={ConstructionWorkEditorRoute.publishers}
+      variant="secondary"
+    />
   </Column>
 )
 
