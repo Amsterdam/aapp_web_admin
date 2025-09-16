541dc5107899e917e66346aac792d04d662575f5
Jeroen Beekman <j.beekman@amsterdam.nl>
2025-09-16 15:33:42 +0200

    Add survey item to management page

---
+++ src/modules/survey/components/SurveyAdminHomeButton.tsx
@@ -0,0 +1,8 @@
+import NavigationButton from 'components/ui/button/NavigationButton'
+import {SurveyRoute} from '../types'
+
+const SurveyAdminHomeButton = () => (
+  <NavigationButton label="Beheer vragenlijsten" url={SurveyRoute.admin} />
+)
+
+export default SurveyAdminHomeButton
---
+++ src/modules/survey/index.ts
@@ -0,0 +1,13 @@
+import SurveyAdminHomeButton from 'modules/survey/components/SurveyAdminHomeButton'
+import {allowedAzureRoles} from 'modules/survey/constants'
+import {routes} from 'modules/survey/routes'
+import {ModuleSlug, ModuleType, type Module} from 'modules/types'
+
+export const surveyModule: Module = {
+  allowedAzureRoles,
+  HomeComponent: SurveyAdminHomeButton,
+  routes,
+  slug: ModuleSlug.survey,
+  title: 'Survey',
+  type: ModuleType.contentManagement,
+}
