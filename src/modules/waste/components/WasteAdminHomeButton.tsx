ec1816574a696b7ad0d1b43466843867f86f6cc7
Jeroen Beekman <j.beekman@amsterdam.nl>
2025-12-09 17:14:26 +0100

    Correct linting

--- src/modules/waste/components/WasteAdminHomeButton.tsx
+++ src/modules/waste/components/WasteAdminHomeButton.tsx
@@ -2,10 +2,7 @@ import NavigationButton from 'components/ui/button/NavigationButton'
 import {WasteRoute} from '../types'
 
 const WasteAdminHomeButton = () => (
-  <NavigationButton
-    label="Beheer A&G notificaties"
-    url={WasteRoute.admin}
-  />
+  <NavigationButton label="Beheer A&G notificaties" url={WasteRoute.admin} />
 )
 
 export default WasteAdminHomeButton
